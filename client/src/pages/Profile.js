3b269033eb281a2a7b15c89998d203d5308f464f
Nairi Hovhannisyan <nairihovhannisyan3@gmail.com>
2025-07-05 23:21:53 +0400

    test

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,24 +9,42 @@ const Profile = () => {
     const { tgUser } = useTelegram();
     const [user, setUser] = useState(null)
     const [showAdditionalInformation, setShowAdditionalInformation] = useState(false);
+    const [loading, setLoading] = useState(true);
+
     const getCurrentUser = async () => {
-      const res = await getUser(tgUser?.id);
-      if (res) setUser(res);
-    }
+      try {
+        const res = await getUser(tgUser?.id);
+        if (res) {
+          setUser(res);
+        } else {
+          setUser(null);
+        }
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
     useEffect(() => {
-      if (!tgUser?.id) return;
-      getCurrentUser();
-    }, [tgUser])
+      if (tgUser?.id) {
+        getCurrentUser();
+      } else {
+        setLoading(false); // If no tgUser, stop loading too
+      }
+    }, [tgUser]);
     return (
       <div className="bg-white shadow-2xl rounded-2xl p-8 max-w-sm text-center">
         {/* <h2 className="text-3xl font-bold text-[#5C1F0C] mb-4">Բարի Գալու՜ստ</h2> */}
     
-        {user ? (
+        {loading ? (
+          <p className="text-gray-600 text-center">Բեռնում է...</p>
+        ) : user ? (
           <div className="text-center">
             <img
               src={user.photo_url}
               alt="User"
-              className="w-24 h-24 rounded-full mx-auto shadow-md mb-4"
+              className="w-[150px] h-[150px] rounded-full mx-auto shadow-md mb-4 border-4 border-[#ffcb02]"
             />
             <h3 className="text-xl font-semibold text-[#5C1F0C] mb-2">
               {user.name}
